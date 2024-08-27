6cb29822b1cc680ab7c42dff77f59d1303346f6d
Adwoa Aboagye <158626661+Adwoa-p@users.noreply.github.com>
2024-08-27 16:02:03 +0000

    modified playlist code

--- MP3_Playlist.py
+++ MP3_Playlist.py
@@ -1,10 +1,23 @@
 class Playlist:
+    def __init__ (self, playlist = []):
+        self.playlist = playlist
+
     def load_playlist(self):
-       return
-    
-    def display_tracks(self):
-        return
+        file = open('glblcd_MP3\playlist.txt','r')
+        read = file.readlines()
+        for line in read:
+            self.playlist.append(line.strip())
 
-    def add_mp3(self):
+    def display_playlist(self):
+        for song in self.playlist: 
+            print(song)
 
+    def add_mp3(self,song):
+        self.playlist.append(song)
+
+my_playlist=Playlist()
+my_playlist.load_playlist()
+# print(my_playlist.display_playlist())
+new=my_playlist.add_mp3("Love you Lord")
+my_playlist.display_playlist()
         
